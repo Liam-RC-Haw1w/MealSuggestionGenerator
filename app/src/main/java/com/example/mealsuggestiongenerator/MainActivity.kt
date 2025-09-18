5c3c9bef18f36f13f2d8828b9aaa6f1938ab3762
Liam-RC-Haw1w <ST10499386@rcconnect.edu.za>
2025-09-18 22:50:29 +0200

    Comments added and changes to else statement
    Added comments for understanding and made changes to else statement when a user inputs something incorrectly in the input box. Provided the list

--- app/src/main/java/com/example/mealsuggestiongenerator/MainActivity.kt
+++ app/src/main/java/com/example/mealsuggestiongenerator/MainActivity.kt
@@ -18,16 +18,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
+// imports needed for the code
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+        setContent { // Variable created for timeofday is what the user will input
             var timeOfDay by remember {
                 mutableStateOf(value = "")
             }
-
+                   // Variable created for the meal suggestions
             var mealSuggestion by remember {
                 mutableStateOf(value = "")
             }
@@ -39,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     onValueChange = { text ->
                         timeOfDay = text
                     },
-                    label = {
+                    label = { // A label so that the user has an example of what to input
                         Text(text ="Enter the time of day currently (Example:Morning")
                     },
                     modifier = Modifier
@@ -48,6 +48,8 @@ class MainActivity : ComponentActivity() {
                 )
 
                 // Buttons style to be next to each other
+                // Outcome when a user inputs time of day
+                // Generate button created
                 Row(
                     modifier = Modifier.padding(vertical = 8.dp),
                     horizontalArrangement = Arrangement.spacedBy(space =8.dp)
@@ -61,13 +63,14 @@ class MainActivity : ComponentActivity() {
                                 "afternoon snack" -> "Cookies and milk"
                                 "dinner" -> "Pasta or Stir Fry"
                                 "after dinner snack" -> "Ice cream or Fruit salad"
-                                else -> "No suggestion available"
+                                else -> "Please enter the following options: Morning, Mid-morning, "+
+                                         "Afternoon, Afternoon Snack, Dinner, After Dinner Snack"
                             }
                         }
-                    ) {
+                    ) { // Button name to get a suggestion
                         Text(text = "Get Suggestion")
                     }
-                    // Button for resetting the text
+                    // Button created for resetting the text
                     // Followed on arc video
                     Button(
                         onClick = {
@@ -79,7 +82,7 @@ class MainActivity : ComponentActivity() {
                     }
                 }
 
-                // Display Suggestion
+                // Displays the suggestion based on the user input
                 Text(text = "Suggestion: $mealSuggestion")
             }
         }
